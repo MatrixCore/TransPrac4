8ca2b2daa0555aed474d107fafc97ac328b4c1ae
Matthew <MDPitcairn@gmail.com>
2019-08-08 17:39:03 +0200

    Initial commit

---
+++ setpath.bat
@@ -0,0 +1,14 @@
+REM    You may have to set the "path" on your own computer
+REM    to be able to see the CSHARP compiler
+
+REM    Running a simple scrpit like this will do the trick for
+REM    youu, or you can modify your default settings
+
+REM    The command below may not work for your system as it stands.
+REM    You may have to use a different folder.  Look in your system
+REM    to find the C:\Microsoft.NET\Framework folder, and use the
+REM    the folder name you see there in place of v4.0.30319
+
+   Path = %path%; c:\WINDOWS\Microsoft.NET\Framework\v4.0.30319;
+
+
